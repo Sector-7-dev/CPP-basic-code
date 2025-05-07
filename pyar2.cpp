ed8c9c0f2ca95fc12d5563c3b3f9584e93cc93f2
Sector-7-dev <keshavmiglani98@gmail.com>
2025-05-07 18:19:55 -0400

    Add files via upload

---
+++ pyar2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int x,y;
+
+    cin >> x >> y;
+
+    cout << "I love you"<<"\n" <<x <<endl <<"I love you"<<y;
+    
+    return 0;
+
+
+
+}
+
